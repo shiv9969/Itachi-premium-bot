ded0521d78e34ff5ed3b6340d4ca16a314c32e2a
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-21 06:02:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -167,11 +167,11 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-    elif query.from_user.id in db.has_premium_access and settings['button']:
+    elif db.has_premium_access(query.from_user.id) and settings['button']:
         btn = []
         for file in files:
             files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('boxoffice') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
-    elif query.from_user.id in db.has_premium_access and not settings["button"]:
+    elif db.has_premium_access(query.from_user.id) and not settings["button"]:
         btn = [
             [
                 InlineKeyboardButton(
@@ -309,11 +309,11 @@ async def language_check(bot, query):
                 ]
                 for file in files
             ]
-        elif query.from_user.id in db.has_premium_access and settings['button']:
+        elif db.has_premium_access(query.from_user.id) and settings['button']:
             btn = []
             for file in files:
                 files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('boxoffice') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
-        elif query.from_user.id in db.has_premium_access and not settings["button"]:
+        elif db.has_premium_access(query.from_user.id) and not settings["button"]:
             btn = [
                 [
                     InlineKeyboardButton(
@@ -448,11 +448,11 @@ async def quality_check(bot, query):
                 ]
                 for file in files
             ]
-        elif query.from_user.id in db.has_premium_access and settings['button']:
+        elif db.has_premium_access(query.from_user.id) and settings['button']:
             btn = []
             for file in files:
                 files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('boxoffice') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
-        elif query.from_user.id in db.has_premium_access and not settings["button"]:
+        elif db.has_premium_access(query.from_user.id) and not settings["button"]:
             btn = [
                 [
                     InlineKeyboardButton(
@@ -580,11 +580,11 @@ async def seasons_check(bot, query):
                 ]
                 for file in files
             ]
-        elif query.from_user.id in db.has_premium_access and settings['button']:
+        elif db.has_premium_access(query.from_user.id) and settings['button']:
             btn = []
             for file in files:
                 files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('boxoffice') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
-        elif query.from_user.id in db.has_premium_access and not settings["button"]:
+        elif db.has_premium_access(query.from_user.id) and not settings["button"]:
             btn = [
                 [
                     InlineKeyboardButton(
@@ -1991,11 +1991,11 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
-    elif message.from_user.id in db.has_premium_access and settings['button']:
+    elif db.has_premium_access(message.from_user.id) and settings['button']:
         btn = []
         for file in files:
             files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('boxoffice') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
-    elif message.from_user.id in db.has_premium_access and not settings["button"]:
+    elif db.has_premium_access(message.from_user.id) and not settings["button"]:
         btn = [
             [
                 InlineKeyboardButton(
