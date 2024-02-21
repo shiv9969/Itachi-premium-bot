25372a7526af46e98df84ad85aed494d07893258
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-21 16:27:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -112,7 +112,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Friend, this movie will not be available, asked for it in the group 👉, @Apnamovie4</b>")
+    await message.reply_text("<b>Friend, this movie will not be available, asked for it in the group 👉, t.me/+xpgODfC0d3o3MDE1</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
