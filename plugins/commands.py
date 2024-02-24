30ba5b580b2f16f1890144c187a2d233a4de952e
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-24 23:53:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -349,7 +349,7 @@ async def start(client, message):
                     f_caption=f_caption
             if f_caption is None:
                 f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
-            if settings['is_shortlink']:
+            if await get_settings['is_shortlink']:
                 settings = await get_settings(chat_id)
                 g = await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=files_{files}")
                 k = await client.send_message(chat_id=message.from_user.id,text=f"<b>📕Nᴀᴍᴇ ➠ : <code>{files.file_name}</code> \n\n🔗Sɪᴢᴇ ➠ : {get_size(files.file_size)}\n\n📂Fɪʟᴇ ʟɪɴᴋ ➠ : {g}\n\n<i>Note: ⚠️ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇᴅ ᴀғᴛᴇʀ 𝟷𝟶 ᴍɪɴᴜᴛᴇs.</i></b>", reply_markup=InlineKeyboardMarkup(
