6ba78eba3c3c839590f444fbca7837fe2e3970fd
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-24 14:00:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -267,7 +267,7 @@ async def language_check(bot, query):
         key = f"{query.message.chat.id}-{query.message.id}"
         files_link = ''
         pre = 'filep' if settings['file_secure'] else 'file'
-        elif settings['button']:
+        if settings['button']:
             btn = []
             for file in files:
                 files_link += f"""<b>\n\n❤️ <a href=https://t.me/{temp.U_NAME}?start=files_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('Linkz') and not x.startswith('{') and not x.startswith('Original') and not x.startswith('Villa') and not x.startswith('boxoffice') and not x.startswith('Links') and not x.startswith('@') and not x.startswith('www'), file.file_name.split()))}</a></b>"""
