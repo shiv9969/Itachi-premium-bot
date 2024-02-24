d017470d1efa5f7faa5db2c50040e6f738f157c8
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-24 22:26:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -366,7 +366,7 @@ async def start(client, message):
                     f_caption=f_caption
             if f_caption is None:
                 f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
-            if IS_VERIFY and not db.has_premium_access(message.from_user.id) and not await check_verification(client, message.from_user.id):
+            if IS_VERIFY and not await check_verification(client, message.from_user.id):
                 btn = [[
                         InlineKeyboardButton("♻️ Vᴇʀɪғʏ ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=")),
                         InlineKeyboardButton("⚠️ Hᴏᴡ Tᴏ Vᴇʀɪғʏ ⚠️", url=HOW_TO_VERIFY)
