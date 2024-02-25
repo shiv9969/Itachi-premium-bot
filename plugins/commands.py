2b278936cbe895223897420100cc91838571c23d
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-25 11:59:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -346,6 +346,7 @@ async def start(client, message):
             )
             return
         else:
+            chat_id = int("-" + file_id.split("-")[1])
             gg = (chat_id, f"https://telegram.me/{temp.U_NAME}?start=allfiles_{file_id}")
             k = await client.send_message(chat_id=message.from_user.id,text=f"<b>Get All Files in a Single Click!!!</i></b>", 
             reply_markup=InlineKeyboardMarkup([[
