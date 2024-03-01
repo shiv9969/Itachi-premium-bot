22f27437aad930a7c90c8d2bf3139b7e0ad2a204
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-03-01 19:52:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -94,7 +94,7 @@ async def private_receive_handler(client, message):
     # file_id = message.document or message.video
     replied = message.reply_to_message
     if replied and replied.media:
-        msg = await message.forward(
+        msg = await client.send_cached_media(
             chat_id=BIN_CHANNEL)
 
    # file_name = file_id.file_name.replace("_", " ").replace(".mp4", "").replace(".mkv", "").replace(".", " ")
