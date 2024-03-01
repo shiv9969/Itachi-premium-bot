9f22020e6b4e66cff5d3f015af22c4cfeaa78f3f
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-03-01 08:06:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
 from info import *
-from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, send_all, get_tutorial, get_shortlink
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, send_all, get_tutorial, get_shortlink, bot_settings, save_bot_settings
 from database.connections_mdb import active_connection
 import re
 import json
@@ -1157,7 +1157,7 @@ async def onshortlink(bot, message):
     if user.status != enums.ChatMemberStatus.ADMINISTRATOR and user.status != enums.ChatMemberStatus.OWNER and str(userid) not in ADMINS:
         return await message.reply_text("<b>ᴏɴʟʏ ᴀᴅᴍɪɴ ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ !</b>")
     else:
-        await save_group_settings(grpid, 'stream_link_mode', True)
+        await save_bot_settings(grpid, 'stream_link_mode', True)
         STREAM_LINK_MODE = True
         return await message.reply_text("ꜱʜᴏʀᴛʟɪɴᴋ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴇɴᴀʙʟᴇᴅ.")
 
