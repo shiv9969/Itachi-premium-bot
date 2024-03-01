d61ad03ff8594f5838ce7bda6d61bdb81f5b1720
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-03-01 23:29:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -89,16 +89,14 @@ async def stream_download(bot, query):
                     ],[
                         InlineKeyboardButton('⁉️ ᴄʟᴏsᴇ ⁉️', callback_data='close_data')]]))
                         
-@Client.on_message(filters.command("stream"))
+@Client.on_message((filters.private) & (filters.document | filters.video) , group=4)
 async def private_receive_handler(client, message):
     file_id = message.document or message.video
-    replied = message.reply_to_message
-    if replied and replied.media:
-        msg = await client.send_cached_media(
-            chat_id=BIN_CHANNEL,
-            file_id=file_id)
 
-   # file_name = file_id.file_name.replace("_", " ").replace(".mp4", "").replace(".mkv", "").replace(".", " ")
+    msg = await message.forward(
+        chat_id=BIN_CHANNEL)
+
+    file_name = file_id.file_name.replace("_", " ").replace(".mp4", "").replace(".mkv", "").replace(".", " ")
 
     online = f"{URL}/watch/{msg.id}"
     download = f"{URL}/download/{msg.id}"
@@ -107,7 +105,7 @@ async def private_receive_handler(client, message):
     await client.send_message(text=f"<b>Requested By: {message.from_user.mention}\nLink:</b>\n{link}", chat_id=BIN_CHANNEL, disable_web_page_preview=True)
 
     await message.reply_text(
-        text=f"<b>Here Is Your Streamable Link\n\n<b>Powered By - <a href=https://t.me/lusifilms>©LUSI FILM 📽️ ™</a></b>",
+        text=f"<b>Here Is Your Streamable Link\n\nFile Name</b>:\n<code>{file_name}</code>\n\n<b>Powered By - <a href=https://t.me/lusifilms>Lusi Films 📽️ ™</a></b>",
         reply_markup=InlineKeyboardMarkup(
         [
             [
