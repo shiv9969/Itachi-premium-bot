2a20fe252d91c9b90670a22d0cef41f40c0fba0e
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-03-01 23:41:56 +0530

    Update render_template.py

--- jk_dev/utils/render_template.py
+++ jk_dev/utils/render_template.py
@@ -24,17 +24,17 @@ async def render_page(message_id):
     audio_formats = ['audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav']
     video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
     if mime_type.lower() in video_formats:
-        async with aiofiles.open('web/template/req.html') as r:
+        async with aiofiles.open('jk_dev/template/req.html') as r:
             heading = 'Watch {}'.format(file_name)
             tag = mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
     elif mime_type.lower() in audio_formats:
-        async with aiofiles.open('web/template/req.html') as r:
+        async with aiofiles.open('jk_dev/template/req.html') as r:
             heading = 'Listen {}'.format(file_name)
             tag = mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
     else:
-        async with aiofiles.open('web/template/dl.html') as r:
+        async with aiofiles.open('jk_dev/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
                 async with s.get(src) as u:
                     file_size = get_size(u.headers.get('Content-Length'))
