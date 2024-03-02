d58746679cd8f1462b0653842fd2c8c324e7af27
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-03-02 21:42:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1876,7 +1876,7 @@ async def auto_filter(client, msg, spoll=False):
         message = msg
         settings = await get_settings(message.chat.id)
         search = message.text
-        files, offset, total_results = await get_search_results(search, offset=0, filter=True)
+        files, offset, total_results = await get_search_results(message.chat.id ,search.lower(), offset=0, filter=True)
         if not files:
             if settings["spell_check"]:
                 return await advantage_spell_chok(msg)
