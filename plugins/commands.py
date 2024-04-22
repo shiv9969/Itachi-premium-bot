ada2fa32f5151ed8007e113dc76e00791f342fec
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-04-22 14:59:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -298,8 +298,9 @@ async def start(client, message):
     elif data.split("-", 1)[0] == "reff":
         user_id = int(data.split("-", 1)[1])
         safari = await referal_add_user(user_id, message.from_user.id)
+        total = await get_referal_users_count(user_id)
         if safari:
-            await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>") 
+            await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot total user {total}</b>") 
             num_referrals = await get_referal_users_count(user_id)
             await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
             if await get_referal_users_count(user_id) == 4:
