12a34191e59355b7ed1c87d3003c55f3ae06b055
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-04-23 08:56:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -300,13 +300,13 @@ async def start(client, message):
         if await db.has_premium_access(user_id):
             await message.reply("You are a premium user, you cannot open the invite link.")
             return
-        elif await db.save_invites(user_id):
+        elif await db.save_invites(user_id, message.from_user.id):
             await message.reply("You are Already Invited")
             return
         else:
             if await referal_add_user(user_id, message.from_user.id):
                 await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot total user</b>") 
-                await db.update_invited(user_id)
+                await db.update_invited(user_id, message.from_user.id)
             num_referrals = await get_referal_users_count(user_id)
             await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
             if await get_referal_users_count(user_id) == USERS_COUNT:
