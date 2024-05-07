e96268850c38c5763ab3c357f8bc9fe3b6dabfe5
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-07 12:17:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,8 +45,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:/")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Lusibot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mogo012:mogo012@cluster0.wkdapvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MINATO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #refer time, or user count
