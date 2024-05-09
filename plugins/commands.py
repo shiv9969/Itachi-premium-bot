3301b90f489abdb4f8c1cfcbc181b4fb5ab627c1
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-09 08:55:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -121,7 +121,7 @@ async def start(client, message):
                 try:
                     kk, file_id = message.command[1].split("_", 1)
                     pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                    btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start=files_{file.file_id}")])
+                    btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
                 except (IndexError, ValueError):
                     btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
             await client.send_message(
