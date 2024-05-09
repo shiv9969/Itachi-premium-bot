3ff1ccde0341d937fb8b48d1fed5e63bd4f5edba
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-09 08:49:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -689,7 +689,7 @@ async def send_all(bot, userid, files, ident):
         btn = [[
                 InlineKeyboardButton("❆ Jᴏɪɴ Oᴜʀ Bᴀᴄᴋ-Uᴘ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link)
             ],[
-                InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"{pre}#send_all")
+                InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"sendfiles#{key}")
             ]]
         await bot.send_message(
             chat_id=userid,
