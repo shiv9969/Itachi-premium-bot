b73296706f14a1e2a6482348b3029fb56e7213ce
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-09 09:41:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -746,7 +746,7 @@ async def send_all(bot, userid, files, ident):
             InlineKeyboardButton("Vᴇʀɪғʏ", url=await get_token(bot, userid, f"https://telegram.me/{temp.U_NAME}?start=", 'send_all')),
             InlineKeyboardButton("Hᴏᴡ Tᴏ Vᴇʀɪғʏ", url=HOW_TO_VERIFY)
             ],[
-            InlineKeyboardButton("💸 𝐑𝐞𝐦𝐨𝐯𝐞 𝐕𝐞𝐫𝐢𝐟𝐲 💸", callback_data='seeplans')
+            InlineKeyboardButton("🏕️ ʀᴇᴍᴏᴠᴇ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴ 🏕️", url="https://t.me/Hidden_Technology_Bot?start=TheHappyHour")
         ]]
         await bot.send_message(
             chat_id=userid,
