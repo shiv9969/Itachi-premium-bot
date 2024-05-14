64d00ed1cefb9f25288b25c1c31d6be1c804d37a
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-05-14 11:40:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1302,7 +1302,7 @@ async def refer(bot, message):
     try:
         user_id = message.from_user.id
         total = db2.get_refer_points(user_id)
-        await message.reply_text(f"<b>ʀᴇғᴇʀʀᴇ ʏᴏᴜʀ ʟɪɴᴋ ᴛᴏ ʏᴏᴜʀ ғʀɪᴇɴᴅs, ғᴀᴍɪʟʏ, ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɢʀᴏᴜᴘ ᴛᴏ ɢᴇᴛ ғʀᴇᴇ ᴘʀᴇᴍɪᴜᴍ ғᴏʀ 1 ᴍᴏɴᴛʜ \n\nʀᴇғᴇʀᴀʟ ʟɪɴᴋ - https://t.me/{temp.U_NAME}?start=reff_{user_id}\n\nsʜᴀʀᴇ ᴛʜɪs ʟɪɴᴋ ᴡɪᴛʜ ʏᴏᴜʀ ғʀɪᴇɴᴅs, ᴇᴀᴄʜ ᴛɪᴍᴇ ᴛʜᴇʏ ᴊᴏɪɴ,  ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ 10 ʀᴇғғᴇʀᴀʟ ᴘᴏɪɴᴛs ᴀɴᴅ ᴀғᴛᴇʀ {USER_POINT} ᴘᴏɪɴᴛs ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ 1 ᴍᴏɴᴛʜ ᴘʀᴇᴍɪᴜᴍ sᴜʙsᴄʀɪᴘᴛɪᴏɴ.\n\nʙᴜʏ ᴘᴀɪᴅ ᴘʟᴀɴ ʙʏ - /premium</b>",
+        await message.reply_text(script.REFFER_TXT.format(temp.U_NAME, user_id, USER_POINT)),
         replymarkup=InlineKeyboardMarkup([
                     [InlineKeyboardButton("invite 🔗", url=f"https://telegram.me/share/url?url=https://t.me/{temp.U_NAME}=reff_{user_id}&text=Hᴇʟʟᴏ%21%20ᴇxᴘᴇʀɪᴇɴᴄᴇ%20ᴀ%20ʙᴏᴛ%20ᴛʜᴀᴛ%20ᴏғғᴇʀs%20ᴀ%20ᴠᴀsᴛ%20ʟɪʙʀᴀʀʏ%20ᴏғ%20ᴜɴʟɪᴍɪᴛᴇᴅ%20ᴍᴏᴠɪᴇs%20ᴀɴᴅ%20sᴇʀɪᴇs.%20%F0%9F%98%83")],
                     [InlineKeyboardButton(f"⏳{total}", callback_data=f"show_reff")],
