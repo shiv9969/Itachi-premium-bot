521fd86474e99b71110bde68fd37d660aa189bae
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-15 08:50:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,8 @@ async def force_sub(client, message):
         user = message.from_user.first_name
         # invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         btn = [[
-                InlineKeyboardButton("Jᴏɪɴ Cʜᴀɴɴᴇʟ", url=f't.me/The_Happy_Hours')
+                InlineKeyboardButton("Cʜᴀɴɴᴇʟ 1", url=f't.me/The_Happy_Hours'),
+                InlineKeyboardButton("Cʜᴀɴɴᴇʟ 2", url=f't.me/The_Happy_Hour_Hindi')
               ]]
         msg=await message.reply_photo(photo='https://i2f9m2t2.rocketcdn.me/wp-content/uploads/2014/04/shutterstock_175386392.jpg',
             caption=f"ꜰɪʀꜱᴛ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀꜰᴛᴇʀ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ....✅\n\n<i>पहले आप हमारे चैनल को ज्वाइन करे फिर वापस सर्च करें</i></b>",
