fa4eadd940ba78bf404e947b8332d057584031f4
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-15 12:37:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,7 +101,7 @@ async def start(client, message):
                 parse_mode=enums.ParseMode.HTML
             )
             return
-        if AUTH_CHANNEL_1 and not await is_subscribed(client, message):
+        if AUTH_CHANNEL and not await is_subscribed(client, message):
             # try:
             #     invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
             # except ChatAdminRequired:
