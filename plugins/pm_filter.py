a943cffcb5c1bfb0a7df5641ded75bfea8811934
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-16 20:34:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2141,12 +2141,12 @@ async def advantage_spell_chok(client, msg):
             await asyncio.sleep(30)
             await k.delete()
             return
-            elif not PM_FILTER and NO_RESULTS_MSG:
-                await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(message.chat.title, message.chat.id, total, temp.B_NAME, reqstr.mention, mv_rqst)))
+        elif not PM_FILTER and NO_RESULTS_MSG:
+            await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(message.chat.title, message.chat.id, total, temp.B_NAME, reqstr.mention, mv_rqst)))
             k = await msg.reply_photo(
-                photo=(SPELL_IMG), 
-                caption=script.I_CUDNT.format(mv_rqst),
-                reply_markup=InlineKeyboardMarkup(button)
+            photo=(SPELL_IMG), 
+            caption=script.I_CUDNT.format(mv_rqst),
+            reply_markup=InlineKeyboardMarkup(button)
             )
             await asyncio.sleep(30)
             await k.delete()
