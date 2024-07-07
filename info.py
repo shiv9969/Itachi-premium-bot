749a9fb274de6d853cf01d1671ba49e8382e672f
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-07-07 18:19:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mogo007:mogo007@cluster0.7o1vfny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Itachi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
