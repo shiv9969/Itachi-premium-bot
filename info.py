9da5e2c73452a7a36f8540dbd6596645bd01f02e
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-02-09 13:07:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ API = environ.get("API", "728ac8e235b7da546190c8a97c73ddedcd8e27cf") # shortlink
 URL = environ.get("URL", "adlinkfly.in") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/kaisekhole/11") # how to open link 
 BOT_USERNAME = environ.get("BOT_USERNAME", "narutossssbot") # bot username without @
-VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+VERIFY = environ.get("VERIFY", "False") # set True Or False and make sure spelling is correct and first letter capital.
 
 #reporting...
 TOP_SEARCH = is_enabled((environ.get('TOP_SEARCH', "False")), False)
