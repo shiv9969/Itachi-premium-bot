03428736103f5152e0ae1775f6adb83deccbd00c
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-02-12 19:31:52 +0530

    Update verification.py

--- plugins/verification.py
+++ plugins/verification.py
@@ -168,9 +168,3 @@ def get_readable_time(seconds):
     return result
 
 verifydb = VerifyDB()
-
-Note ⚠️: Add This Code In Root Of Your Repo Or Anywhere You Want.
-Credit : @GK-BOTZ
-'''
-    
-                
