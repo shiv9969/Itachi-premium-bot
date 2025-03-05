2280eea32db223c4bf3ee35d25bbaac8a6fb0247
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-05 18:19:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -210,8 +210,8 @@ async def pm_text(bot, message):
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
     await message.reply_text(
-         text=f"<b><a href='https://t.me/Movies_Request_Group_10'>Pleasen Request your fav movies & series in our movies request group </a></b>",   
-         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📚 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 📚", url=f"https://t.me/Movies_Request_Group_10")]])
+         text=f"<b><a href='https://t.me/BoB_AreA'>Pleasen Request your fav movies & series in our movies request group /n/n jion the group and request whatever you want </a></b>",   
+         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📚 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 📚", url=f"https://t.me/BoB_AreA")]])
     )
     await bot.send_message(
         chat_id=LOG_CHANNEL,
